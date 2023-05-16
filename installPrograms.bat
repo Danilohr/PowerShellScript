077acf640fcd5dc117c48d22076a937ea1992fbe
Danilohr <75738653+Danilohr@users.noreply.github.com>
2023-05-16 15:53:04 -0300

    v2

--- installPrograms.bat
+++ installPrograms.bat
@@ -1,3 +1,2 @@
 powershell.exe -ExecutionPolicy Bypass -Command "powershell.exe -File '%~dp0\installPrograms.ps1'" -Wait
-powershell.exe -ExecutionPolicy Bypass -Command "Start-Process '%~dp0..\Ninite NET.exe'"
 pause
--- teste.ps1
+++ teste.ps1
@@ -1,9 +1,9 @@
-do{
-$erro = 'false'
-	try{
+cls
+echo '============================================================================'
+
 # Baixa o winget
 try{
-	if(winget) {"Instalador de aplicativos já existente, prosseguindo"}
+	if(winget) {"Instalador de aplicativos ja existente, prosseguindo"}
 }
 catch{
 	try{
@@ -18,35 +18,78 @@ catch{
 	Invoke-WebRequest -Uri $URL -OutFile "Setup.msix" -UseBasicParsing
 
 	# install
-    	Start-Process -FilePath .\"Setup.msix"
-    	do{
-        Start-Sleep -Seconds 1
-    	} while ((Get-Process "AppInstaller" -ErrorAction SilentlyContinue) -ne $null)
+		Start-Process -FilePath .\"Setup.msix"
+		do{
+		Start-Sleep -Seconds 1
+		} while ((Get-Process "AppInstaller" -ErrorAction SilentlyContinue) -ne $null)
 
 	# delete file
 	Remove-Item "Setup.msix"
 	
 	echo 'Gerenciador de pacotes instalado'
 	}
-	catch {pause}
+	catch {
+		echo 'Verifique a conexão com a internet e tente novamente' 
+		pause}
 }
 
-# Programas
-winget install -e --id Apache.OpenOffice --accept-source-agreements --silent
-winget install -e --id Microsoft.Teams --accept-source-agreements --silent
-winget install -e --id Adobe.Acrobat.Reader.32-bit --accept-source-agreements --silent
-winget install -e --id Oracle.JavaRuntimeEnvironment --accept-source-agreements --silent
 
-# Programas do Ninite
-winget install -e --id Mozilla.Firefox.ESR --accept-source-agreements --silent
-winget install -e --id Microsoft.Edge --accept-source-agreements --silent
-winget install -e --id 7zip.7zip --accept-source-agreements --silent
-winget install -e --id Notepad++.Notepad++ --accept-source-agreements --silent
-winget install -e --id CodecGuide.K-LiteCodecPack.Standard --accept-source-agreements --silent
-#  winget install -e --id TeamViewer.TeamViewer --accept-source-agreements --silent
-winget install -e --id Google.Chrome --accept-source-agreements --silent
+$programs = 
+    'Apache.OpenOffice',
+    #'Microsoft.Teams',
+    'Adobe.Acrobat.Reader.32-bit',
+	'Oracle.JavaRuntimeEnvironment',
+	'Mozilla.Firefox.ESR',
+	'Microsoft.Edge',
+	'7zip.7zip',
+	'Notepad++.Notepad++',
+	'CodecGuide.K-LiteCodecPack.Standard',
+	#'TeamViewer.TeamViewer',
+	'Google.Chrome'
 
-	}catch{
-	cls
-	$erro = 'true'}
-}while ($erro = 'true')
+for($i = 0 ; $i -lt $programs.count ; $i++){
+	$nome = $($programs[$i].split('.')[1])
+	echo "- $($nome):"
+	
+	for($erro = 0 ; $erro -lt 3 ;){
+		winget install -h -e --id $programs[$i] --accept-source-agreements --ignore-checksum
+		$result = winget list -e --id $programs[$i]
+		
+		if($result -like "Nenhum pacote instalado foi encontrado"){
+			echo "Tentando instalar novamente"
+		}
+		else {
+			$erro = 3
+		}
+		$erro++
+	}
+}
+
+
+# Java 32 bit
+$string = winget list 'Java'
+$count = (Select-String -InputObject $string -Pattern 'Java 8 Update *' -AllMatches).Matches.Count
+if($count -le 1){
+	Invoke-WebRequest -UseBasicParsing -Uri "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=248240_ce59cff5c23f4e2eaf4e778a117d4c5b" -OutFile "../java32Bit.exe"
+	Start-Process -FilePath "..\java32Bit.exe"
+	echo 'instalador do java deve ter aberto'
+	# Tentar remover o javatemp.exe
+}
+
+# Remove o Office
+winget uninstall -h -e --id Microsoft.MicrosoftOfficeHub_8wekyb3d8bbwe
+try {
+	#winget uninstall -h -e --id 'O365HomePremRetail - pt-br' --accept-source-agreements
+	Start-Process powershell.exe -ArgumentList "winget uninstall -h -e --id 'O365HomePremRetail - pt-br' --accept-source-agreements"
+}
+catch {pause}
+
+# Ninite e Teams
+Start-Process -FilePath '\\ihm.local\arquivos\SUPORTE TECNICO\Formatacao\Ninite NET.exe'
+Start-Process -FilePath '\\ihm.local\arquivos\SUPORTE TECNICO\Formatacao\TeamsSetup_c_w_.exe'
+
+# NetExtender
+echo "201.48.224.194:4433"
+Set-Clipboard "201.48.224.194:4433 LocalDomain"
+echo "LocalDomain"
+winget install -i --id 'SonicWALL.NetExtender'
